c71d2a97a4598a4f013ae0a8161d326b4af84a17
lhespel <44492009+lhespel@users.noreply.github.com>
2018-10-26 12:01:51 +0200

    update main.c

--- main.c
+++ main.c
@@ -8,10 +8,6 @@ int addition(int a, int b){
 
 int main()
 {
-<<<<<<< HEAD
-    printf("a+b=%d\n", addition(1,4));
-=======
     printf("L'addition de a et de b vaut : %d\n", addition(1,4));
->>>>>>> d43370bc1b5b63ce9b2c50384861cf1424dac505
     return 0;
 }
